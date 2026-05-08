214d0a18b06da1d49641aeef663d9d08b53657e1
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2026-05-08 23:50:43 +0530

    Initial Commit

---
+++ Widgets.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Apps\Tms\Components\System\Filters;
+
+use System\Base\Providers\ModulesServiceProvider\Modules\Components\ComponentsWidgets;
+
+class Widgets extends ComponentsWidgets
+{}
